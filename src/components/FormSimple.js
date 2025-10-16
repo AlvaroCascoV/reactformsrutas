7a6d2578995edafb377a458bb97c41978f2c61ad
AlvaroCascoV <alvarocascovalero@gmail.com>
2025-10-16 09:16:27 +0200

    extra formsimple

--- src/components/FormSimple.js
+++ src/components/FormSimple.js
@@ -12,11 +12,19 @@ export default class FormSimple extends Component {
     //PARA RECUPERAR EL VALOR DE UN OBJETO React:
     let nombre = this.cajaNombre.current.value;
     console.log("Nombre recibido: " + nombre)
+    this.setState({
+      nombre: nombre
+    })
+  }
+
+  state = {
+    nombre:""
   }
 
   render() {
     return (<div>
       <h1>Formulario simple React</h1>
+      <h2 style={{color:"blue"}}>{this.state.nombre}</h2>
       <form onSubmit={this.peticionFormulario}>
         <label>Nombre: </label>
         <input type='text' ref={this.cajaNombre} />
